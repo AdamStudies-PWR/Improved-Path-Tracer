e056c74c0e14adcc88919235078648258c80c566
Adam Krizar <adam.krizar@gmail.com>
2024-02-10 19:12:00 +0100

    Prototype refractive reflection

--- src/scene/objects/Sphere.cpp
+++ src/scene/objects/Sphere.cpp
@@ -35,7 +35,7 @@ double Sphere::intersect(const Ray& ray) const
     else delta = sqrt(delta);
 
     return (intersection = -b - delta) > MARGIN
-        ? intersection : ((intersection = -b + delta) > MARGIN ? intersection : 0);
+        ? intersection : ((intersection = -b + delta) > MARGIN ? intersection : 0.0);
 }
 
 RayData Sphere::calculateReflections(const Ray& ray, const Vec3& intersection, const uint16_t depth,
@@ -74,51 +74,33 @@ RayData Sphere::calculateSpecular(const containers::Ray& ray, const containers::
     auto normal = (intersection - position_).norm();
     normal = ray.direction_.dot(normal) < 0 ? normal * -1 : normal;
     auto reflectedDirection = ray.direction_ - normal * 2 * ray.direction_.dot(normal);
-    return {{Ray(intersection, reflectedDirection), 0.9}};
+    return {{Ray(intersection, reflectedDirection), 1.0}};
 }
 
 RayData Sphere::calculateRefractive(const Ray& ray, const Vec3& intersection, std::mt19937& generator,
     const uint16_t depth) const
 {
     Vec3 normal = (intersection - position_).norm();
-    Vec3 surfaceNormal = normal.dot(ray.direction_) < 0 ? normal : normal * -1;
+    normal = (ray.direction_.dot(normal) < 0 ? normal * -1 : normal) * -1;
 
-    Ray reflected = calculateSpecular(ray, intersection)[0].first; // hack
+    auto specular = ray.direction_ - normal * 2 * ray.direction_.dot(normal);
 
-    bool isEntering = normal.dot(surfaceNormal) > 0;
-    const auto localIOR = isEntering ? 1/GLASS_IOR : GLASS_IOR;
-    const auto scalar = ray.direction_.dot(surfaceNormal);
-    const auto cos2t = 1 - localIOR*localIOR*(1 - scalar * scalar);
-
-    if (cos2t < 0) return {{reflected, 0.0}};
-
-    Ray refracted = (ray.direction_ * localIOR - normal * ((isEntering ? 1 : -1) * (scalar * localIOR + sqrt(cos2t))))
-        .norm();
-
-    auto normalReflectence = (GLASS_IOR - 1) * (GLASS_IOR - 1)/((GLASS_IOR + 1) * (GLASS_IOR + 1));
-    auto cTheta = 1 - (isEntering ? -localIOR : refracted.direction_.dot(normal));
-
-    auto fresnelReflectence = normalReflectence + (1 - normalReflectence) * pow(cTheta, 5);
-
-    auto probablilty = 0.25 + 0.5 * fresnelReflectence;
-
-    auto reverseFresnel = 1 - fresnelReflectence;
-    auto reflectenceProbability = fresnelReflectence/probablilty;
-    auto reverseProbability = reverseFresnel/(1 - probablilty);
-
-    if (depth > 2)
+    if (depth < 2)
     {
-        if (zero_one(generator) < probablilty)
-        {
-            return {{reflected, reflectenceProbability}};
-        }
-        else
-        {
-            return {{refracted, reverseProbability}};
-        }
+        return {
+            {Ray(intersection, specular * -1), 0.92},
+            {Ray(intersection, specular), 0.08}
+        };
     }
 
-    return {{reflected, fresnelReflectence}, {refracted, reverseFresnel}};
+    if (zero_one(generator) > 0.92)
+    {
+        return {{Ray(intersection, specular), 1.0}};
+    }
+    else
+    {
+        return {{Ray(intersection, specular * -1), 1.0}};
+    }
 }
 
 }  // namespace tracer::scene::objects
