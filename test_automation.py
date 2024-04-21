f159628651d0e6e3480cdf7632fe48003a7df7be
Adam Krizar <adam.krizar@gmail.com>
2024-04-21 20:40:28 +0200

    Fixed issues with lighting

---
+++ test_automation.py
@@ -0,0 +1,19 @@
+import os
+import subprocess
+
+EXECUTABLE = "./tracer"
+
+DEPTHS = [10]
+SAMPLES = [40, 80, 200, 400, 1000, 2000, 5000]
+SCENES = ["scenes/maze.json", "scenes/mirrors.json", "scenes/spheres.json"]
+
+if not os.path.exists("tracer"):
+    print("Executable not found")
+    exit(1)
+
+
+for scene in SCENES:
+    for depth in DEPTHS:
+        for sample in SAMPLES:
+            print("\nStarting: " + scene + " Depth=" + str(depth) + " Samples=" + str(sample))
+            subprocess.run([EXECUTABLE + " -d="+str(depth) + " -s="+str(sample) + " " + scene], shell=True, check=True)
