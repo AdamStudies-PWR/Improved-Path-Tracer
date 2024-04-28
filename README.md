162aa38aa032dca96cb43c13acbd4490eac104a5
Adam Krizar <adam.krizar@gmail.com>
2024-04-28 15:12:22 +0200

    Added option to run single test case through test automation Updated README (removed mentions of valgrind program) Updated .gitignore to exclude benchmark.txt file

--- README.md
+++ README.md
@@ -25,15 +25,31 @@ tracer --help
 
 To display up to date help message.
 
+## Output
+
+On output two files are created:
+* sceneNameDXSY.png - rendered image.
+* benchmark.txt - Contains render id (sceneNameDXSY) and information on how long render took. If this file exists new result will be appended to it
+
 ## Memory profiling
 
-To determin memory usage use valgrind.
+To automate testing and analyze memory usage use test_automation.py
 
 ```
-valgrind --tool=massif ./tracer -d=10 -s=40 scenes/spheres.json
+python3 test_automation.py
 ```
 
-This command generates massif output file which can be later analyzed using *massif-visualizer*
+Runing it without arguments will run all test cases (40, 80, 200, 400, 1000, 2000, 5000 samples | 10 depth |) for each scene.
+
+Using argument [-o / --one] will enable execution of single test case.
+
+To specify scene parametres (only works in single test mode) use parametres:
+* [-d / --depth] - pases argument to -d argument of main program [Default: 10]
+* [-s / --samples] - pases argument to -s argument of main program [Default: 40]
+* [-p / --path] - specify which scene should be used for single test [Default: scene/spheres.json]
+
+Results will be appended to the benchmark.txt file.
+
 
 ## Requirements
 ImageMagic needs to be installed from source using: https://softcreatr.github.io/imei/
--- test_automation.py
+++ test_automation.py
@@ -1,3 +1,4 @@
+import argparse
 import os
 import resource
 import subprocess
@@ -13,6 +14,15 @@ SAMPLES = [40, 80, 200, 400, 1000, 2000, 5000]
 SCENES = ["scenes/maze.json", "scenes/mirrors.json", "scenes/spheres.json"]
 
 
+parser = argparse.ArgumentParser(
+    prog="TraceBench",
+    description="Benchamrk tool and memory profiler for tracer program.")
+parser.add_argument('-o', '--one', action='store_false', help='Enable execution of single test case.')
+parser.add_argument('-s', '--samples', default="40", help="Specifies number of samples per pixel.")
+parser.add_argument('-d', '--depth', default="10", help="Specifies max number of reflections per ray.")
+parser.add_argument('-p', '--path', default="scenes/spheres.json", help="Specifies path to json file with scene data.")
+
+
 def to_mebibytes(kibibytes) -> str:
     return str(round(kibibytes/1024, 2))
 
@@ -20,25 +30,37 @@ def to_mebibytes(kibibytes) -> str:
 def test(scene, depth, sample):
     print("\nStarting: " + scene + " Depth=" + str(depth) + " Samples=" + str(sample))
     subprocess.check_call([EXECUTABLE + " -d="+str(depth) + " -s="+str(sample) + " " + scene], shell=True)
-    max_mem = (resource.getrusage(resource.RUSAGE_CHILDREN).ru_maxrss)
+    maxrss = (resource.getrusage(resource.RUSAGE_CHILDREN).ru_maxrss)
+    mib_used = to_mebibytes(maxrss)
+    print("Memory used: " + mib_used + " MiB")
     with open(BENCHMARK_FILE, 'a') as file:
-        file.write(to_mebibytes(max_mem) + "\n")
+        file.write(mib_used + "\n")
 
 
-def main():
-    if not os.path.exists("tracer"):
-        print("Executable not found")
-        exit(1)
-
-    if os.path.exists(BENCHMARK_FILE):
-        os.remove(BENCHMARK_FILE)
-
+def test_many():
     for scene in SCENES:
         for depth in DEPTHS:
             for sample in SAMPLES:
                 p = Process(target=test, args=(scene, depth, sample))
                 p.start()
                 p.join()
+
+
+def main() -> int:
+    if not os.path.exists("tracer"):
+        print("Executable not found")
+        return 1
+
+    if os.path.exists(BENCHMARK_FILE):
+        os.remove(BENCHMARK_FILE)
+
+    args = parser.parse_args()
+    if not args.one:
+        test(args.path, args.depth, args.samples)
+    else:
+        test_many()
+
+    return 0
 
 
 if __name__ == '__main__':
