695417bed6921ecda50ab310fb6ff0e7673f0449
Adam Krizar <adam.krizar@gmail.com>
2024-05-26 15:21:45 +0200

    Added auto killing test after timeout (24h), changed default order of scenes in automated testing

--- test_automation.py
+++ test_automation.py
@@ -1,19 +1,23 @@
 import argparse
 import os
 import resource
+import signal
 import subprocess
 
-from multiprocessing import Process
+from multiprocessing import Process, Manager
 
 
 NVIDIA_SMI_CMD = ["nvidia-smi", "--query-compute-apps=name,used_memory", "--format=csv", "-lms=500"]
+NVIDIA_SMI_CHECK_PID_CMD = ["nvidia-smi", "--query-compute-apps=name,pid", "--format=csv"]
 
 EXECUTABLE = "./tracer"
 BENCHMARK_FILE = "benchmark.txt"
 
+TIMEOUT = 86400
+
 DEPTHS = [10]
-SAMPLES = [40, 80, 200, 400, 1000, 2000, 5000]
-SCENES = ["scenes/maze.json", "scenes/mirrors.json", "scenes/spheres.json"]
+SAMPLES = [40, 80, 200, 400, 1000, 2000, 5000, 10000]
+SCENES = ["scenes/spheres.json", "scenes/maze.json", "scenes/mirrors.json"]
 
 
 parser = argparse.ArgumentParser(
@@ -23,6 +27,13 @@ parser.add_argument('-o', '--one', action='store_false', help='Enable execution 
 parser.add_argument('-s', '--samples', default="40", help="Specifies number of samples per pixel.")
 parser.add_argument('-d', '--depth', default="10", help="Specifies max number of reflections per ray.")
 parser.add_argument('-p', '--path', default="scenes/spheres.json", help="Specifies path to json file with scene data.")
+
+
+def write_timeout(scene, sample, depth):
+    id = scene.split("/")[1].split(".")[0] + "D" + str(depth) + "S" + str(sample)
+    line = id + ";DNF;DNF;DNF"
+    with open(BENCHMARK_FILE, 'a') as file:
+        file.write(line + "\n")
 
 
 def to_mebibytes(kibibytes) -> str:
@@ -49,35 +60,69 @@ def get_gpu_usage(output) -> str:
     return str(max_mem)
 
 
+def kill_dangling_process():
+    out = str(subprocess.check_output(NVIDIA_SMI_CHECK_PID_CMD)).split('\\n')
+    for entry in out:
+        data = entry.split(", ")
+        if len(data) != 2:
+            continue
+        if data[0] != EXECUTABLE:
+            continue
+        os.kill(int(data[1]), signal.SIGTERM)
 
-def test(scene, depth, sample):
+
+def test(scene, depth, sample, result):
     print("Starting: " + scene + " Depth=" + str(depth) + " Samples=" + str(sample))
-    subprocess.check_call([EXECUTABLE + " -d="+str(depth) + " -s="+str(sample) + " " + scene], shell=True)
+    result[0] = True
+
+    proc = subprocess.Popen([EXECUTABLE + " -d="+str(depth) + " -s="+str(sample) + " " + scene], shell=True)
+    try:
+        proc.wait(timeout=TIMEOUT)
+    except subprocess.TimeoutExpired:
+        print("\nTimeout! Skipping further execution for scene/depth combination.\n")
+        kill_dangling_process()
+        result[0] = False
+
     maxrss = (resource.getrusage(resource.RUSAGE_CHILDREN).ru_maxrss)
-    mib_used = to_mebibytes(maxrss)
+    result[1] = to_mebibytes(maxrss)
+
+
+def run_test(scene, depth, sample):
+    manager = Manager()
+    result = manager.dict()
+    gpu_memory = subprocess.Popen(NVIDIA_SMI_CMD, shell=False, stdout=subprocess.PIPE)
+    p = Process(target=test, args=(scene, depth, sample, result))
+    p.start()
+    p.join()
+    gpu_memory.kill()
+
+    if not result[0]:
+        write_timeout(scene, sample, depth)
+        return True
+
+    mib_used = result[1]
     print("CPU Memory used: " + mib_used + " MiB")
     with open(BENCHMARK_FILE, 'a') as file:
         file.write(mib_used + ";")
 
-
-def prepare_test(scene, depth, sample):
-    gpu_memory = subprocess.Popen(NVIDIA_SMI_CMD, shell=False, stdout=subprocess.PIPE)
-    p = Process(target=test, args=(scene, depth, sample))
-    p.start()
-    p.join()
-    gpu_memory.kill()
     output = gpu_memory.stdout.read()
     peak_gpu = get_gpu_usage(output)
     print("GPU Memory used: " + peak_gpu + " MiB\n")
     with open(BENCHMARK_FILE, 'a') as file:
         file.write(peak_gpu + "\n")
+    return False
 
 
 def test_many():
+    too_long: bool
     for scene in SCENES:
         for depth in DEPTHS:
+            too_long = False
             for sample in SAMPLES:
-                prepare_test(scene, depth, sample)
+                if not too_long:
+                    too_long = run_test(scene, depth, sample)
+                else:
+                    write_timeout(scene, sample, depth)
 
 
 def main() -> int:
@@ -90,7 +135,7 @@ def main() -> int:
 
     args = parser.parse_args()
     if not args.one:
-        prepare_test(args.path, args.depth, args.samples)
+        run_test(args.path, args.depth, args.samples)
     else:
         test_many()
 
